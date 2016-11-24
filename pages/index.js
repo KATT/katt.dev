48e57ca7e75b07e0e9b31d41a4e59b7641b09cd2
KATT <alex@dice.fm>
2016-11-24 15:18:26 +0100

    :tada: init innit

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import Link from 'next/link';
-import css from 'next/css';
+import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+import styleSheet from 'styled-components/lib/models/StyleSheet';
 
-const style = css({
-  transition: 'all .4s',
-  height: '100vh',
-  color: '#fff',
-  background: '#2ecc71',
-  ':hover': {
-    background: '#27ae60',
-  },
-  '@media (max-width: 600px)': {
-    background: '#3498db',
-    ':hover': {
-      background: '#2980b9',
-    },
-  },
+import Head from 'next/head';
+
+const globalCSS = `
+  html, body {
+    height: 100%;
+  }
+  body {
+    background: #f0f0f0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+
+const KATTCORP_LOGO = `\
+             / )
+ / )__/ )___/ /
+( @ . @ )     )
+ (           )
+ //"//""//"//
+
+ KATTCORP LTD.`;
+
+const index = props => (
+  <Wrapper>
+    <Head>
+      <title>KATTCORP LTD.</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      {props.isServer && (
+        <style>
+          {globalCSS}
+          {styleSheet.rules().map(rule => rule.cssText).join('\n')}
+        </style>
+      )}
+    </Head>
+    <PRE>{KATTCORP_LOGO}</PRE>
+    <UL>
+      <LI><a href="https://github.com/KATTCORP">github</a></LI>
+      <LI><a href="https://twitter.com/alexheartjs">twitter</a></LI>
+      <LI><a href="https://www.linkedin.com/in/johanssonalexander">linkedin</a></LI>
+      <LI><a href="mailto: alexander@n1s.se">email</a></LI>
+    </UL>
+  </Wrapper>
+);
+
+index.propTypes = {
+  isServer: PropTypes.bool.isRequired,
+};
+
+index.getInitialProps = ({ req }) => ({
+  isServer: !!req,
 });
 
-export default () => (
-  <div className={style}>
-    Home:<Link href="/about">About</Link>
-  </div>
-);
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+const PRE = styled.pre`
+  text-align: left;
+  display: inline-block;
+`;
+const UL = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`;
+const LI = styled.li`
+  flex: 1;
+  margin: 0;
+  padding: 0 5px;
+`;
+
+export default index;
