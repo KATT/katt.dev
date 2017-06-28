511ea95360d2e3553765b3f5ed9611fd06c0b944
KATT <alexander@n1s.se>
2017-06-28 23:37:58 +0100

    :sparkles: google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
 // @flow
 
-import Head from 'next/head';
+import Layout from '../components/Layout';
 import LinkList from '../components/LinkList';
 import Logo from '../components/Logo';
 
-
 export default () => (
-  <div>
-    <Head>
-      <title>KATTCORP LTD.</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+  <Layout>
     <Logo />
     <LinkList items={[
       { href: 'https://github.com/KATTCORP', title: 'github' },
@@ -19,23 +14,5 @@ export default () => (
       { href: 'mailto: alexander@n1s.se', title: 'email' },
     ]}
     />
-    <style jsx>{`
-      div {
-        text-align: center;
-      }
-    `}</style>
-    <style global jsx>{`
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      body {
-        background: #f0f0f0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    `}</style>
-  </div>
+  </Layout>
 );
