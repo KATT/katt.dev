4c2440aefa9961ed3c6811c1f5e005965e1a46a4
KATT <alexander@n1s.se>
2019-02-08 16:57:41 -0600

    ⚡ jsonld update

--- utils/jsonld.tsx
+++ utils/jsonld.tsx
@@ -1,34 +1,35 @@
 export const JSONLD_KATTCORP = {
-  "@context": "http://schema.org",
-  "@type": "Organization",
-  name: "KATTCORP",
-  legalName: "KATTCORP LTD.",
-  url: "http://kattcorp.co.uk",
-  foundingDate: "2016",
+  '@context': 'http://schema.org',
+  '@type': 'Organization',
+  name: 'KATTCORP',
+  legalName: 'KATTCORP LTD.',
+  url: 'http://kattcorp.co.uk',
+  foundingDate: '2016-10-26',
   founders: [
     {
-      "@type": "Person",
-      name: "Alexander Johansson"
-    }
+      '@type': 'Person',
+      name: 'Alexander Johansson',
+    },
   ],
   address: {
-    "@type": "PostalAddress",
-    streetAddress: "900 Linton Blvd Suite 104",
-    addressRegion: "London",
-    postalCode: "E8",
-    addressCountry: "United Kingdom"
+    '@type': 'PostalAddress',
+    streetAddress: 'C/O Oliver Phillips Ltd, 133 Whitechapel High Street',
+    addressRegion: 'London',
+    postalCode: 'E1 7QA',
+    addressCountry: 'United Kingdom',
   },
   contactPoint: {
-    "@type": "ContactPoint",
-    contactType: "sales",
-    email: "alex@n1s.se",
-    url: "https://twitter.com/alexheartjs"
+    '@type': 'ContactPoint',
+    contactType: 'sales',
+    email: 'alex@n1s.se',
+    url: 'https://twitter.com/alexheartjs',
   },
   sameAs: [
-    "https://github.com/KATT",
-    "https://github.com/KATTCORP",
-    "https://www.linkedin.com/in/johanssonalexander/",
-    "https://www.linkedin.com/company/19376256/",
-    "https://twitter.com/alexheartjs"
-  ]
+    'https://github.com/KATT',
+    'https://github.com/KATTCORP',
+    'https://www.linkedin.com/in/johanssonalexander/',
+    'https://www.linkedin.com/company/19376256/',
+    'https://twitter.com/alexheartjs',
+    'https://beta.companieshouse.gov.uk/company/10447485',
+  ],
 };
