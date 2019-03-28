686740a665fb1e6214eeed148090235bf6ab474a
KATT <alexander@n1s.se>
2019-03-28 11:50:41 -0300

    ⚡ static build

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+
+module.exports = {
+  ...withTypescript(),
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" }
+    };
+  }
+};
