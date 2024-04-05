462d85ebcb088b6993aadecb1084fc4be609002c
KATT <alexander@n1s.se>
2024-04-05 14:36:30 +0200

    meep

--- src/app/shiki/page.tsx
+++ src/app/shiki/page.tsx
@@ -1,7 +1,5 @@
 import { Code } from '@/ui/Code';
-import { unstable_cache } from 'next/cache';
 
-const code = unstable_cache(async function () {}, ['codeToHtml']);
 export default async function Page() {
   return <Code code={`console.log()`} />;
 }
