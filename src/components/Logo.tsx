854606ca176f8e70ff32d8cd60c0f872a02e4fc1
KATT <alexander@n1s.se>
2024-04-05 12:05:53 +0200

    cool

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,9 +11,11 @@ const KATTCORP_LOGO = `
 export function Logo() {
   return (
     <div>
+      <h1 className="sr-only">KATTCORP</h1>
       <pre
         dangerouslySetInnerHTML={{ __html: KATTCORP_LOGO }}
         className="text-left inline-block text-sm leading-tight"
+        aria-hidden
       />
     </div>
   );
