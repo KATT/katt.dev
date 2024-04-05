66761c79cd40bea1c277321f7fa70d82bb23c297
KATT <alexander@n1s.se>
2024-04-05 12:15:06 +0200

    coool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,20 @@ import { Metadata } from 'next';
 
 export default function Page() {
   return (
-    <>
-      <main className="flex flex-col items-center justify-center flex-1 space-y-4">
-        <Logo />
-        <LinkList
-          items={[
-            { href: 'https://github.com/KATT', title: 'GitHub' },
-            { href: 'https://twitter.com/alexdotjs', title: 'Twitter' },
-            {
-              href: 'https://linkedin.com/in/johanssonalexander/',
-              title: 'LinkedIn',
-            },
-            { href: 'mailto: alex@kattcorp.com', title: 'Email' },
-          ]}
-        />
-      </main>
-    </>
+    <main className="flex flex-col items-center justify-center flex-1 space-y-4">
+      <Logo />
+      <LinkList
+        items={[
+          { href: 'https://github.com/KATT', title: 'GitHub' },
+          { href: 'https://twitter.com/alexdotjs', title: 'Twitter' },
+          {
+            href: 'https://linkedin.com/in/johanssonalexander/',
+            title: 'LinkedIn',
+          },
+          { href: 'mailto: alex@kattcorp.com', title: 'Email' },
+        ]}
+      />
+    </main>
   );
 }
 
