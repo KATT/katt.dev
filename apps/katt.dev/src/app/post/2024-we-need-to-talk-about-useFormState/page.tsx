b86eb2cc8a7ee5be1363dd539d101049d93b32ab
KATT <alexander@n1s.se>
2024-04-06 23:17:49 +0200

    wip

--- apps/katt.dev/src/app/post/2024-we-need-to-talk-about-useFormState/page.tsx
+++ apps/katt.dev/src/app/post/2024-we-need-to-talk-about-useFormState/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Tweet } from "react-tweet";
 
 export default function Page() {
@@ -7,6 +8,27 @@ export default function Page() {
         <h1>
           We need to talk about <code>useFormState()</code>
         </h1>
+        <div>
+          <time dateTime="2024-04-06">April 6, 2024</time>
+        </div>
+        <div className="not-prose">
+          <a
+            href="https://twitter.com/jullerino"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="avatar__photo-link"
+          >
+            <Image
+              src="https://github.com/KATT.png"
+              width={50}
+              height={50}
+              alt="Alex Johansson"
+              className="rounded-full"
+            />
+          </a>
+        </div>
+
+        <p>Hi</p>
 
         <div className="not-prose">
           <Tweet id="1775835646491283604" />
