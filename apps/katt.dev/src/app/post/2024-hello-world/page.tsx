8b734afad70d101a546d361466531ef378693c8b
KATT <alexander@n1s.se>
2024-04-07 22:55:04 +0200

    cool

--- apps/katt.dev/src/app/post/2024-hello-world/page.tsx
+++ apps/katt.dev/src/app/post/2024-hello-world/page.tsx
@@ -1,15 +1,22 @@
+import { JsonLd } from "@/ui/JsonLd";
 import { Tweet } from "react-tweet";
 import { PostAuthor } from "../ui/PostAuthor";
-import { JsonLd } from "@/ui/JsonLd";
 
 const postDate = "2024-04-06";
+
+const headline = "Hello world";
+const author: React.ComponentProps<typeof PostAuthor> = {
+  name: 'Alex "KATT" Johansson',
+  title: "Creator of tRPC",
+  avatarSrc: "https://github.com/KATT.png",
+  href: "https://twitter.com/alexdotjs",
+};
+
 export default function Page() {
   return (
     <>
       <article className="prose dark:prose-invert">
-        <h1>
-          What's up with the <code>state</code> of <code>useFormState</code>?
-        </h1>
+        <h1>{headline}</h1>
         <div>
           <time dateTime={postDate}>
             {Intl.DateTimeFormat("en-US", {
@@ -27,25 +34,20 @@ export default function Page() {
         </div>
 
         <div className="not-prose">
-          <PostAuthor
-            name='Alex "KATT" Johansson'
-            title="Creator of tRPC"
-            avatarSrc="https://github.com/KATT.png"
-            href="https://twitter.com/alexdotjs"
-          />
+          <PostAuthor {...author} />
         </div>
       </article>
       <JsonLd
         data={{
           "@context": "https://schema.org",
           "@type": "BlogPosting",
-          headline: "What's up with the state of useFormState?",
-          datePublished: "2024-04-06",
-          dateModified: "2024-04-06",
+          headline,
+          datePublished: postDate,
+          dateModified: postDate,
           author: {
             "@type": "Person",
-            name: 'Alex "KATT" Johansson',
-            url: "https://twitter.com/alexdotjs",
+            name: author.name,
+            url: author.href,
           },
         }}
       />
