bfa7508581743ec25e04a6b5de5c990307e199eb
KATT <alexander@n1s.se>
2024-04-08 00:02:44 +0200

    svg logo ish

--- apps/katt.dev/src/ui/Logo.tsx
+++ apps/katt.dev/src/ui/Logo.tsx
@@ -8,14 +8,24 @@ const KATTCORP_LOGO = `
   KATTCORP AB`;
 
 export function Logo() {
+  const fontSize = 12;
+  const lineHeights = fontSize * 1.25;
+  const lines = KATTCORP_LOGO.split("\n");
+  const height = lines.length * lineHeights;
+  const width = lines.reduce((max, line) => Math.max(max, line.length), 0) * 10;
   return (
-    <div>
-      <h1 className="sr-only">KATTCORP</h1>
-      <pre
-        dangerouslySetInnerHTML={{ __html: KATTCORP_LOGO }}
-        className="text-left inline-block text-sm leading-tight"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${width} ${height}`}
         aria-hidden
-      />
-    </div>
+        className="stroke-inherit fill-current w-40 h-40"
+        
+      >
+        {KATTCORP_LOGO.split("\n").map((line, i) => (
+          <text key={i} x="0" y={i * 14} className="font-mono text-sm">
+            {line.replaceAll(" ", "\u00A0")}
+          </text>
+        ))}
+      </svg>
   );
 }
