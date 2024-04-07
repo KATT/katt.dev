2acec009336dc19fd9e110cf1702f5ffefa3c997
KATT <alexander@n1s.se>
2024-04-08 00:03:10 +0200

    cool

--- apps/katt.dev/src/ui/Logo.tsx
+++ apps/katt.dev/src/ui/Logo.tsx
@@ -14,18 +14,17 @@ export function Logo() {
   const height = lines.length * lineHeights;
   const width = lines.reduce((max, line) => Math.max(max, line.length), 0) * 10;
   return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox={`0 0 ${width} ${height}`}
-        aria-hidden
-        className="stroke-inherit fill-current w-40 h-40"
-        
-      >
-        {KATTCORP_LOGO.split("\n").map((line, i) => (
-          <text key={i} x="0" y={i * 14} className="font-mono text-sm">
-            {line.replaceAll(" ", "\u00A0")}
-          </text>
-        ))}
-      </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${width} ${height}`}
+      aria-hidden
+      className="stroke-inherit fill-current w-40 h-40"
+    >
+      {KATTCORP_LOGO.split("\n").map((line, i) => (
+        <text key={i} x="0" y={i * 14} className="font-mono text-sm">
+          {line.replaceAll(" ", "\u00A0")}
+        </text>
+      ))}
+    </svg>
   );
 }
