0fd815043bde03b929c25d803df27a70f37d0327
Alex / KATT <alexander@n1s.se>
2024-04-08 02:17:23 +0200

    🎨 svg logo ish (#21)

--- apps/katt.dev/src/ui/Logo.tsx
+++ apps/katt.dev/src/ui/Logo.tsx
@@ -8,14 +8,33 @@ const KATTCORP_LOGO = `
   KATTCORP AB`;
 
 export function Logo() {
+  const fontSize = 12;
+  const lineHeights = fontSize * 1.25;
+  const lines = KATTCORP_LOGO.split("\n");
+  const height = lines.length * lineHeights;
+  const lineWidth = lines.reduce((max, line) => Math.max(max, line.length), 0);
+  const width = lineWidth * 8.5;
+
+  const logoWithWhite = lines.map((line) =>
+    line.replaceAll(" ", " ").padEnd(lineWidth, " ")
+  );
+
   return (
-    <div>
-      <h1 className="sr-only">KATTCORP</h1>
-      <pre
-        dangerouslySetInnerHTML={{ __html: KATTCORP_LOGO }}
-        className="text-left inline-block text-sm leading-tight"
-        aria-hidden
-      />
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${width} ${height}`}
+      aria-hidden
+      className="stroke-inherit fill-current font-mono text-sm"
+      style={{
+        width: `${width}px`,
+      }}
+      aria-label="an ASCII cat"
+    >
+      {logoWithWhite.map((line, i) => (
+        <text key={i} y={i * 14} x="50%" text-anchor="middle">
+          {line.replaceAll(" ", "\u00A0")}
+        </text>
+      ))}
+    </svg>
   );
 }
